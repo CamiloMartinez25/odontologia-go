1cf1a443b1137ff48a896847542fd73715347398
Cecilia Espada <69773045+ceciespada@users.noreply.github.com>
2023-09-13 21:44:43 -0300

    Agregando función requestToPaciente y POST Paciente

--- internal/domain/paciente/service.go
+++ internal/domain/paciente/service.go
@@ -1,1 +1,23 @@
 package paciente
+
+func (s *service) Create(ctx context.Context, requestPaciente RequestPaciente) (Paciente, error) {
+	paciente := requestToPaciente(requestPaciente)
+	response, err := s.repository.Create(ctx, paciente)
+	if err != nil {
+		log.Println("Error en service Paciente: Método Create")
+		return Paciente{}, errors.New("Error en service Paciente: Método Create")
+	}
+
+	return response, nil
+}
+
+func requestToPaciente(requestPaciente RequestPaciente) Paciente {
+	var paciente Paciente
+	paciente.Nombre = requestPaciente.Nombre
+	paciente.Apellido = requestPaciente.Apellido
+	paciente.Domicilio = requestPaciente.Domicilio
+	paciente.DNI = requestPaciente.DNI
+	paciente.FechaAlta = requestPaciente.FechaAlta
+
+	return paciente
+}
