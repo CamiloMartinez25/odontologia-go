cb6ae4b4d1bbcb95587ba9cb7b027a22ca68d123
Camilo  Martinez <camilo_2510@hotmail.com>
2023-09-13 20:03:34 -0500

    adding update method implementation

--- internal/domain/odontologo/repository.go
+++ internal/domain/odontologo/repository.go
@@ -5,6 +5,10 @@ import (
 	"database/sql"
 )
 
+var (
+	ErrNotFound  = errors.New("product not found")
+)
+
 type repository struct {
 	db *sql.DB
 }
@@ -13,4 +17,37 @@ func NewRepositoryMySql(db *sql.DB) Repository {
 	return &repository{
 		db: db,
 	}
+}
+
+
+// Update updates an odontologo.
+func (r *repository) Update(ctx context.Context, ondontologo Odontologo) (Odontologo, error) {
+	statement, err := r.db.Prepare(QueryUpdateOdontologo)
+	if err != nil {
+		return Odontologo{}, err
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec(
+		odontologo.Nombre,
+		odontologo.Apellido,
+		odontologo.Matricula,
+		odontologo.ID,
+	)
+
+	if err != nil {
+		return Odontologo{}, err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return Odontologo{}, err
+	}
+
+	if rowsAffected < 1 {
+		return Odontologo{}, ErrNotFound
+	}
+
+	return odontologo, nil
 }
--- internal/domain/odontologo/service.go
+++ internal/domain/odontologo/service.go
@@ -18,9 +18,32 @@ type Service interface {
 	Delete(ctx context.Context, id int) error
 }
 
-// NewService creates a new product service.
+// NewService creates a new odontologo service.
 func NewService(repository Repository) Service {
 	return &service{
 		repository: repository,
 	}
+}
+
+
+// Update updates an odontologo.
+func (s *service) Update(ctx context.Context, requestOdontologo RequestOdontologo, id int) (Odontologo, error) {
+	odontologo := requestToodontologo(requestOdontologo)
+	odontologo.ID = id
+	response, err := s.repository.Update(ctx, odontologo)
+	if err != nil {
+		log.Println("log de error en service de odontologo", err.Error())
+		return Odontologo{}, errors.New("error en servicio. Metodo Update")
+	}
+
+	return response, nil
+}
+
+func requestToOdontologo(requestOdontologo RequestOdontologo) Odontologo {
+	var odontologo Odontologo
+	odontologo.Nombre = requestodOdontologo.Nombre
+	odontologo.Apellido = requestodOdontologo.Apellido
+	odontologo.Matricula = requestodOdontologo.Matricula
+
+	return odontologo
 }
