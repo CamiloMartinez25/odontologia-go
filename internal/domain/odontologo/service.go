744bd998e056baffa7ffecb933ad59f027b95b65
PatoAlloco <patoalloco13@hotmail.com>
2023-09-13 21:37:07 -0300

    model-query-repository-service

--- internal/domain/odontologo/service.go
+++ internal/domain/odontologo/service.go
@@ -1,1 +1,26 @@
 package odontologo
+
+import (
+	"context"
+	"errors"
+	"log"
+)
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	Create(ctx context.Context, requestOdontologo RequestOdontologo) (Odontologo, error)
+	GetAll(ctx context.Context) ([]Odontologo, error)
+	GetByID(ctx context.Context, id int) (Odontologo, error)
+	Update(ctx context.Context, requestOdontologo RequestOdontologo, id int) (Odontologo, error)
+	Delete(ctx context.Context, id int) error
+}
+
+// NewService creates a new product service.
+func NewService(repository Repository) Service {
+	return &service{
+		repository: repository,
+	}
+}
