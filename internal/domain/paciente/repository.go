d423d9ee480acaeeedf4ef9d048839a5792f33f8
Cecilia Espada <69773045+ceciespada@users.noreply.github.com>
2023-09-13 22:09:11 -0300

    Agregando creación de repositorio y método create de un paciente

--- internal/domain/paciente/repository.go
+++ internal/domain/paciente/repository.go
@@ -1,1 +1,50 @@
 package paciente
+
+import (
+	"context"
+	"database/sql"
+)
+
+type repository struct {
+	db *sql.DB
+}
+
+// NewRepositoryMySql crea un nuevo repositorio.
+func NewRepositoryMySql(db *sql.DB) Repository {
+	return &repository{
+		db: db,
+	}
+}
+
+// Create crea un nuevo paciente.
+func (r *repository) Create(ctx context.Context, paciente Paciente) (Paciente, error) {
+
+	statement, err := r.db.Prepare(QueryInsertPaciete)
+
+	if err != nil {
+		return Paciente{}, ErrStatement
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec(
+		paciente.Nombre, 
+		paciente.Apellido, 
+		paciente.Domicilio,
+		paciente.DNI,
+		paciente.FechaAlta, 
+	)
+
+	if err != nil {
+		return Paciente{}, ErrExec
+	}
+
+	lastId, err := result.LastInsertId()
+	if err != nil {
+		return Paciente{}, ErrLastId
+	}
+
+	paciente.ID = int(lastId)
+
+	return paciente, nil
+}
