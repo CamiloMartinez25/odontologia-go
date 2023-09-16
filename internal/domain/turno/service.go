125a88d6e7dc1d82bfe892b18e9e62954be046ea
unknown <sabridileva@gmail.com>
2023-09-15 21:45:58 -0300

    PUT turnos

--- internal/domain/turno/service.go
+++ internal/domain/turno/service.go
@@ -2,6 +2,7 @@ package turno
 
 import (
 	"context"
+	"errors"
 	"log"
 )
 
@@ -35,6 +36,29 @@ func (s *service) GetAll(ctx context.Context) ([]Turno, error) {
 	}
 
 	return turnos, nil
+}
+
+// Update updates an turno.
+func (s *service) Update(ctx context.Context, requestTurno RequestTurno, id int) (Turno, error) {
+	turno := requestToTurno(requestTurno)
+	turno.ID = id
+	response, err := s.repository.Update(ctx, turno)
+	if err != nil {
+		log.Println("log de error en service de turno", err.Error())
+		return Turno{}, errors.New("error en turno. Metodo Update")
+	}
+
+	return response, nil
+}
+
+func requestToTurno(requestTurno RequestTurno) Turno {
+	var turno Turno
+	turno.Paciente = requestTurno.Paciente
+	turno.Odontologo = requestTurno.Apellido
+	turno.FechaHora = requestTurno.FechaHora
+	turno.Descripcion = requestTurno.Descripcion
+
+	return turno
 }
 
 // GetByPacienteID returns a list of turnos according to paciente's ID.
